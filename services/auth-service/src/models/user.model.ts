af62ec46619c1e69905e17383c847358eeb2ec31
Muhammad faisal <muhammadfaisal7864309@gmail.com>
2026-05-21 10:50:50 +0500

    user model creation

---
+++ services/auth-service/src/models/user.model.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+interface IUser {
+  fullName: string;
+  email: string;
+  password: string;
+  role: "User" | "Admin" | "Worker";
+}
+
+const userScehme = new mongoose.Schema<IUser>(
+  {
+    fullName: {
+      type: String,
+      trim: true,
+      required: [true, "Full name is required"],
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+      required: [true, "Password is required"],
+    },
+    role: { type: String, enum: ["User", "Admin", "Worker"], default: "User" },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model<IUser>("User", userScehme);
+
+export default User;
